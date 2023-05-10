69440d02001ff76ee853060911c5d69427e42f0b
Mateusz Łapiński <mateuszlapinski.ml@gmail.com>
2023-05-10 22:24:10 +0200

    Hangman game

---
+++ src/main/java/org/example/Wisielec.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Wisielec {
+
+    // Wyświetlanie alfabetu w konsoli
+    // walidacji inputu
+    // rysunek w konsoli
+    // otwarcie w oknie
+    List<String> words=List.of("samochód", "kod", "programowanie", "komputer");
+    String word;
+    char[] userWord;
+    int lives= 7;
+
+    public void play(){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        word  = words.get(random.nextInt(words.size()));
+        userWord=new char[word.length()];
+
+
+        Arrays.fill(userWord,'_');
+
+
+        while(!gameEnded()){
+            System.out.println(userWord);
+            System.out.println();
+            System.out.println("Podaj kolejną literę: ");
+            System.out.println("Pozostało "+ lives + " żyć");
+
+            //sprawdzenie czy użytkownik podał tylko jedną literę.
+            char letter = scanner.nextLine().charAt(0);
+
+            checkLetter(letter);
+
+        }
+
+        if(lives==0){
+            System.out.println("Niestety nie udało Ci się wygrać");
+        }else {
+            System.out.println("Brawo udało Ci się odgadnąć słowo");
+        }
+
+        scanner.close();
+    }
+
+    private void checkLetter(char letter) {
+        boolean foundLetter=false;
+        for (int i=0; i<word.length(); i++){
+            if (word.charAt(i)==letter){
+                userWord[i]=letter;
+                foundLetter=true;
+            }
+        }
+        if(!foundLetter){
+            System.out.println("Niestety Nie ma takiej litery :(");
+            lives--;
+        }
+    }
+
+    private boolean gameEnded() {
+        return lives== 0 || word.equals(String.valueOf(userWord));
+    }
+}
